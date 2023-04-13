b21e8431d4dfa7d5924eb78c66dc0305eb5485be
sharan sharma <sharansharma19@gmail.com>
2023-04-13 19:23:12 +0530

    added logic for json payload

--- handlers/orderHandler.go
+++ handlers/orderHandler.go
@@ -3,10 +3,19 @@ package handlers
 import (
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/sharansharma94/product-order-system/order"
 )
+
+type UpdateOrderStatusRequest struct {
+	Status string `json:"status"`
+}
+
+type UpdateOrderDispatchDateRequest struct {
+	DispatchDate string `json:"dispatch_date"`
+}
 
 func GetOrderDetailsHandler(orderService *order.OrderService) gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -20,23 +29,50 @@ func GetOrderDetailsHandler(orderService *order.OrderService) gin.HandlerFunc {
 		}
 	}
 }
-
 func UpdateOrderStatusHandler(orderService *order.OrderService) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		orderID, _ := strconv.Atoi(c.Param("orderID"))
-		newStatus := order.Status(c.PostForm("status"))
+		var requestData UpdateOrderStatusRequest
+		err := c.BindJSON(&requestData)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid request data"})
+			return
+		}
+
+		newStatus := order.Status(requestData.Status)
 
 		orderService.UpdateOrderStatus(orderID, newStatus)
 		c.JSON(http.StatusOK, gin.H{"message": "Order status updated."})
 	}
 }
-
 func UpdateDispatchDateHandler(orderService *order.OrderService) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		orderID, _ := strconv.Atoi(c.Param("orderID"))
-		dispatchDate := c.PostForm("dispatch_date")
+		var requestData UpdateOrderDispatchDateRequest
+		err := c.BindJSON(&requestData)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid request data"})
+			return
+		}
 
-		orderService.UpdateDispatchDate(orderID, dispatchDate)
+		_, err = time.Parse("2006-01-02", requestData.DispatchDate)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid date format"})
+			return
+		}
+
+		orderService.UpdateDispatchDate(orderID, requestData.DispatchDate)
 		c.JSON(http.StatusOK, gin.H{"message": "Dispatch date updated."})
 	}
 }
+
+// func UpdateDispatchDateHandler(orderService *order.OrderService) gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		orderID, _ := strconv.Atoi(c.Param("orderID"))
+// 		dispatchDate := c.PostForm("dispatch_date")
+
+// 		orderService.UpdateDispatchDate(orderID, dispatchDate)
+// 		c.JSON(http.StatusOK, gin.H{"message": "Dispatch date updated."})
+// 	}
+// }
